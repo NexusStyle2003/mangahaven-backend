6e510fad2b0a48f44d7b9c6e57fa011d17284ddf
NexusStyle2003 <157353015+NexusStyle2003@users.noreply.github.com>
2025-01-29 17:32:18 +0530

    Update keys_prod.js

--- config/keys_prod.js
+++ config/keys_prod.js
@@ -1,4 +1,4 @@
-module.exports = {
-  mongoURI: process.env.MONGO_URI,
+export default {
+  mongoURI: process.env.MONGO_URI || "mongodb+srv://solankivinubhai971:<db_password>@cluster0.jpomx.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0",
   secretOrKey: process.env.SECRET_OR_KEY,
 };
